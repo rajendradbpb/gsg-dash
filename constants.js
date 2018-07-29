6e9f8e6b03fe74bf50711d4ea5a43ee04d9cb1d1
Rajendra <rajendrasahoodbpb@gmail.com>
2018-07-29 18:39:27 +0530

    - updated api key

--- constants.js
+++ constants.js
@@ -9,7 +9,7 @@ app.constant("Constants", {
         "getUsername" : function() {return this.storagePrefix + "username";},
         "getPassword" : function() {return this.storagePrefix + "password";},
         "getIsRemember" : function() {return this.storagePrefix + "isRemember";},
-        "googleApi":"https://maps.googleapis.com/maps/api/geocode/json?latlng={{latLng}}&key=AIzaSyBKH-ESKu39LGz3Q9Uc4GIZTUdhAlhl0gE",
+        "googleApi":"https://maps.googleapis.com/maps/api/geocode/json?latlng={{latLng}}&key=AIzaSyAO2BvR7qPt-MYMu-P0AT15Ip5DF4jAo1A",
         "hashKey" : "goAppAccount",
         "envData" : {
           "env":"dev",
